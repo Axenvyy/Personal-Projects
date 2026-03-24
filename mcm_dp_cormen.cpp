ff4066b4c3fc4bcbb3e0ff388c96bcef73b8512b
Axenvyy <gparmar_be24@thapar.edu>
2026-03-24 15:46:20 +0000

    working mcm from mcm_dp_cormen

--- mcm_dp_cormen.cpp
+++ mcm_dp_cormen.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void mcm(vector<int> p, vector<vector<int>>& s, vector<vector<int>>& m);
+void mcm(const vector<int>& p, vector<vector<int>>& s, vector<vector<int>>& m);
     void print(const vector<vector<int>>& s,int i, int j);
 
 
@@ -18,30 +18,27 @@ int main(){
     int k;
     cin>> k;
 
-    vector<vector<int>> m;
-    m.reserve(k);
-    for(auto& x:m){x.reserve(k);}
-    vector<vector<int>> s;
-    s.reserve(k);
-    for(auto& x:s){x.reserve(k);}
+    vector<vector<int>> m(k,vector<int>(k,0));
+    vector<vector<int>> s(k,vector<int>(k,0));
+    
    
-    vector<int> p(k-1);
+    vector<int> p(k);
     for( int& x: p){x=r(gen);}
 
     
     mcm(p,s,m);
-    print(s,1,k);
+    print(s,0,k-2);
 
 }
 
-    void mcm(vector<int> p,vector<vector<int>>& s,vector<vector<int>>& m){int n= p.size()-1;
+    void mcm(const vector<int>& p,vector<vector<int>>& s,vector<vector<int>>& m){int n= p.size()-1;
     for (int i=0;i<n;i++){m[i][i]=0;}
     for (int l=1;l<n;l++){
-        for (int i=0;i<n-2+l;i++){
-            int j=i+l-1;
+        for (int i = 0; i < n - l; i++){
+            int j=i+l;
             m[i][j]=INT_MAX;
             for (int l=i;l<j;l++){
-                int q= m[i][l]+m[l+1][j]+p[i-2]*p[l-1]*p[j-1];
+                int q= m[i][l]+m[l+1][j]+p[i]*p[l+1]*p[j+1];
                 if(q<m[i][j]){m[i][j]=q;s[i][j]=l;}
             }
         }
@@ -49,7 +46,7 @@ int main(){
 }
 
 void print(const vector<vector<int>>& s,int i, int j){
-    if(i==j){cout<<i<<" ";}
+    if(i==j){cout<<"A"<<i<<" ";}
     else {cout<<"(";print(s,i,s[i][j]);print(s,s[i][j]+1,j);cout<<")";}
 }
     
