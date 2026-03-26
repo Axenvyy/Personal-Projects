9eb13d8c5453d705aa40e0bf3328a5adbbc723d4
Axenvyy <gparmar_be24@thapar.edu>
2026-03-26 15:44:43 +0000

    self implemented proto type mcm(mcm_self)

--- mcm_self.cpp
+++ mcm_self.cpp
@@ -14,17 +14,17 @@ int main(){
     mt19937 gen(rd());
     uniform_int_distribution<> r(2,100);
 
-    vector<int> p(n-1,0);
+    vector<int> p(n+1,0);
     for(int& x:p){x=r(gen);}
 
-    vector<vector<int>> c(n-1,vector<int>(n-1,INT_MAX));
-    for(int i=0;i<n-1;i++){c[i][i]=0;}
-    for(int k=0;k<n-1;k++){
+    vector<vector<int>> c(n,vector<int>(n,INT_MAX));
+    for(int i=0;i<n;i++){c[i][i]=0;}
+    for(int k=0;k<n;k++){
         for(int j=0;;j++){
-            if(j+k>n-1){break;}
+            if(j+k>=n){break;}
             for(int q=0;q<k;q++){
                 int s= c[j][j+q]+c[j+q+1][j+k]+p[j]*p[j+q+1]*p[j+k+1];
-                if(c[j][j+k]<s){c[j][k+j]=s;}
+                if(c[j][j+k]>s){c[j][k+j]=s;}
 
         }
     }
--- vectors_playground.cpp
+++ vectors_playground.cpp
@@ -39,9 +39,29 @@ int main(){
     mt19937 gen(rd());
     uniform_int_distribution<> r(1,20);
 
-    auto a=rrvector(gen,r);
-    print(a);
+    auto a1=rrvector(gen,r);
+    print(a1);
     
+    vector<vector<int>> a;
 
+    //a=vector<vector<int>>(5,vector<int>(5,0));
+    // now edit by index, kind of reserved space
 
+    //indexes go as a(0,0) to a(4,4)
+    for( int k=0;k<2;k++){
+    for (int i=0;i<5;i++){
+        //for n rows from 0 to n-1 we will pushback a vector with f(i) dimensions 
+        //here f(i)=i+1
+    a.push_back(vector<int>(i+1,0));}}
+
+    vector<vector<int>> b;
+
+    for(int i=0;i<11;i++){
+        int k=25-((i-5)*(i-5)); if(k!=0){
+        b.push_back(vector<int>(k,0));}
+    }
+
+    cout<<b.size();
+    print(b);
+    print(a);
 }
