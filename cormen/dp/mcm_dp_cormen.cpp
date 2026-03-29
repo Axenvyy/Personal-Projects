57df15f44da23ca3a7a6ac06bafb22bececfa846
Axenvyy <gparmar_be24@thapar.edu>
2026-03-29 14:41:10 +0000

    elementary graph algorithms under cormen begin

---
+++ cormen/dp/mcm_dp_cormen.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <utility>
+#include <climits>
+
+using namespace std;
+
+void mcm(const vector<int>& p, vector<vector<int>>& s, vector<vector<int>>& m);
+    void print(const vector<vector<int>>& s,int i, int j);
+
+
+int main(){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> r(2,100);
+
+    int k;
+    cin>> k;
+
+    vector<vector<int>> m(k,vector<int>(k,0));
+    vector<vector<int>> s(k,vector<int>(k,0));
+    
+   
+    vector<int> p(k);
+    for( int& x: p){x=r(gen);}
+
+    
+    mcm(p,s,m);
+    print(s,0,k-2);
+
+}
+
+    void mcm(const vector<int>& p,vector<vector<int>>& s,vector<vector<int>>& m){int n= p.size()-1;
+    for (int i=0;i<n;i++){m[i][i]=0;}
+    for (int l=1;l<n;l++){
+        for (int i = 0; i < n - l; i++){
+            int j=i+l;
+            m[i][j]=INT_MAX;
+            for (int l=i;l<j;l++){
+                int q= m[i][l]+m[l+1][j]+p[i]*p[l+1]*p[j+1];
+                if(q<m[i][j]){m[i][j]=q;s[i][j]=l;}
+            }
+        }
+    }
+}
+
+void print(const vector<vector<int>>& s,int i, int j){
+    if(i==j){cout<<"A"<<i;}
+    else {cout<<"(";print(s,i,s[i][j]);print(s,s[i][j]+1,j);cout<<")";}
+}
+    
+    
---
+++ cormen/elemgraph/adj_list.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+//erdos - renyi model
+
+vector<vector<int>> randomgraph(int v){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> r(0,1);
+
+    double p= r(gen);
+
+    cout<<"generating graph with "<<v<<" vertices with p = "<<p<<"\n";
+
+    vector<vector<int>> g(v);
+
+    for(int i=0;i<v;i++){
+        for(int j=0;j<v;j++){
+        double p_e=r(gen);
+        if (p_e>p){g[i].push_back(j);}
+        
+
+    }
+
+} cout<<"graph generated\n";
+return g;}
+
+void printg(const vector<vector<int>>& g){
+    cout<<"printing graph g with "<<g.size()<<" vertices\n";
+    for(vector<int> v:g){for (int e:v){cout<<e<<" ";}cout<<"\n";}
+}
+
+int main(){
+
+    auto g= randomgraph(50);
+    printg(g);
+
+    
+
+}
