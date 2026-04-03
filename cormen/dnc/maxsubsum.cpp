a58d5fceefb93225b3ae39d10cf3225926880b46
Axenvyy <gparmar_be24@thapar.edu>
2026-04-03 05:00:29 +0000

    updating maxsubsum in dnc/maxsubsum

--- cormen/dnc/maxsubsum.cpp
+++ cormen/dnc/maxsubsum.cpp
@@ -5,45 +5,50 @@
 #include <climits>
 using namespace std;
 
-int kadane(const vector<int>& arr, int& begin, int& end ){
+int kadane(const vector<int>& arr, int& start, int& end ){
+
 
     int n=arr.size();
     int acc=0;
+    int bestl;
     int max=INT_MIN;
     for(int i=0;i<n;i++){
         if(acc<0){
-            acc=arr[i];
-            begin=i;
+            acc=0;
+            start=i;
         }
-        int bestl,bestr;
+        acc+=arr[i];
+        
 
         if(acc>max){
-            max=acc;bestl=begin;bestr=i;
+            max=acc;bestl=start;end=i;
 
         }
-
+       
          
     }
-    return acc;
+   
+    return max;
 }
 
 int main(){
 
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> r(-20,50);
+    uniform_int_distribution<> r(-20,10);
 
     vector<int> arr(500,0);
     cout<<"vector is ";
-    for(int& x:arr){x=r(gen);cout<<x<<" ";}
+    int i=0;
+    for(int& x:arr){x=r(gen);cout<<"("<<i<<")"<<x<<" ";i++;}
     cout<<"\n";
-    int start,endd;
+    int start=0;int endd=0;
 
-    /*int* begin; int* end;
+    int* begin; int* end;
     int acc=0;
     begin=&arr[0];
     for(int i=0;i<500;i++){
-        if(acc<0){acc=0;begin=&arr[i];}
+        if(acc<0){acc=0;begin=&arr[i];start=i;}
         acc+=arr[i];
         
 
@@ -52,7 +57,7 @@ int main(){
 
     end=&arr[499];
     for(int i=499;i<=0;i--){
-        if(acc<0){acc=0;end=&arr[i];}
+        if(acc<0){acc=0;end=&arr[i];endd=i;}
         acc+=arr[i];
         
 
@@ -61,12 +66,14 @@ int main(){
     auto it = begin;
    
     int sum=0;
-    while(it!=end){sum+=*it;it++;}
+    while(it<=end){sum+=*it;it++;}
 
-    cout<<"max subarray sum is "<<sum<<".\n";*/
+    cout<<"max subarray sum is "<<sum<<".\n"<<"with index "<<start<<" and "<<endd<<"\n\n";
+    endd=0;start=0;
+    int maxx=kadane(arr,start,endd);
 
 
-    cout<<"max by kadane's algo is "<<kadane(arr,start,endd)<<"./n with index "<<start<<" and "<<endd<<"\n";
+    cout<<"max by kadane's algo is "<<maxx<<".\n with index "<<start<<" and "<<endd<<"\n";
     
 
 
